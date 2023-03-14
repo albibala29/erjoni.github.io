9254975cdaceb7351385d95ac59c9d4b0a342bbc
albibala29 <91032556+albibala29@users.noreply.github.com>
2023-03-14 09:54:00 -0400

    1st upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById('mobileMenuSwitch');
+
+function openMenu() {
+    menu.classList.remove("visible")
+}
+
+function closeMenu() {
+    menu.classList.add("visible")
+}
